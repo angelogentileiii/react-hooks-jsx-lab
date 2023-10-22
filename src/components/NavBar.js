ab99c8f4d56194bb980a44bbc69925f11840ab29
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-10-22 16:41:36 -0400

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  return <nav>NavBar
+    <a href="StringContaining #home">Home</a>
+    <a href="StringContaining #about">About</a>
+  </nav>;
 }
 
 export default NavBar;
